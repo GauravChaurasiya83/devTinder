6753c9e8f9d87a1afd99e1fbf93e3661a4afd211
GauravChaurasiya83 <gauravchaurasiya870@gmail.com>
2025-05-08 21:49:00 +0530

    exploring GET, POST, DELETE

--- src/app.js
+++ src/app.js
@@ -3,19 +3,25 @@ const { Long } = require("mongodb")
 
 const app = express()
 
+//this will only handle GET call to /user
+app.get("/user",(req,res)=>{
+    res.send({"firstname":"Gaurav","lastname":"Chaurasiya"})
+})
+
+app.post("/user",(req,res)=>{
+    //saving the data to db 
+    res.send("Data of user save successfully to database")
+})
+
+app.delete("/user",(req,res)=>{
+    //deleting the data to db 
+    res.send("Data of user deleted successfully to database")
+})
+
+// this will match all HTTP method API calls to /test
 app.use("/test",(req,res)=>{
-    res.send("hello from the server from test url")
+    res.send("hello from the server")
 })
-
-app.use("/hello",(req,res)=>{
-    res.send("hello hello hello");
-})
-
-app.use("/",(req,res)=>{
-    res.send("hello from the server to the dashboard")
-})
-
-
 
 
 app.listen(3000,()=>{

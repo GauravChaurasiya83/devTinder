2ba76b1d7c8365f0d85d48ed2a88241069e9bc10
GauravChaurasiya83 <gauravchaurasiya870@gmail.com>
2025-05-09 10:51:58 +0530

    playing with multiple route handlers

--- src/app.js
+++ src/app.js
@@ -3,26 +3,39 @@ const { Long } = require("mongodb")
 
 const app = express()
 
-//this will only handle GET call to /user
-app.get("/user",(req,res)=>{
-    res.send({"firstname":"Gaurav","lastname":"Chaurasiya"})
-})
+// //this will only handle GET call to /user
+// app.get("/user",(req,res)=>{
+//     res.send({"firstname":"Gaurav","lastname":"Chaurasiya"})
+// })
 
-app.post("/user",(req,res)=>{
-    //saving the data to db 
-    res.send("Data of user save successfully to database")
-})
+// app.post("/user",(req,res)=>{
+//     //saving the data to db 
+//     res.send("Data of user save successfully to database")
+// })
 
-app.delete("/user",(req,res)=>{
-    //deleting the data to db 
-    res.send("Data of user deleted successfully to database")
-})
+// app.delete("/user",(req,res)=>{
+//     //deleting the data to db 
+//     res.send("Data of user deleted successfully to database")
+// })
 
-// this will match all HTTP method API calls to /test
-app.use("/test",(req,res)=>{
-    res.send("hello from the server")
-})
+// // this will match all HTTP method API calls to /test
+// app.use("/test",(req,res)=>{
+//     res.send("hello from the server")
+// })
 
+app.use("/user",(req,res,next)=>{
+    console.log("Handling the route user!");
+    next()
+},
+(req,res,next)=>{
+    console.log("Handling the route user 2!");
+    next()
+},
+(req,res,next)=>{
+    console.log("Handling the route user 3!");
+    res.send("3rd response")
+}
+)
 
 app.listen(3000,()=>{
     console.log("server started successfully at port 3000");

4fe589ffd80e892a686e4c992b0f33f8674ac4fe
GauravChaurasiya83 <gauravchaurasiya870@gmail.com>
2025-06-30 16:15:22 +0530

    made user feed and added paginaton

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,7 @@ const express = require("express")
 const userRouter = express.Router()
 const {userAuth} = require("../middlewares/auth")
 const {ConnectionRequestModel} = require("../models/connectionRequest")
+const {User} = require("../models/user")
 const { connections } = require("mongoose")
 
 const USER_SAFE_DATA = "firstName lastName age gender skills about"
@@ -23,8 +24,6 @@ userRouter.get("/user/requests/received",userAuth,async(req,res)=>{
         res.status(400).send("ERROR: " + err.message)
     }
 })
-
-
 
 userRouter.get("/user/conections",userAuth,async(req,res)=>{
     try{
@@ -51,6 +50,42 @@ userRouter.get("/user/conections",userAuth,async(req,res)=>{
 
         res.json({message:"connections fetched successfully",data})
 
+    }catch(err){
+        res.status(400).send("ERROR: " + err.message)
+    }
+})
+
+userRouter.get("/user/feed",userAuth,async(req,res)=>{
+    try{
+
+        const page = parseInt(req.query.page) || 1
+        let limit = parseInt(req.query.limit) || 10
+
+        limit = limit > 50 ? 50 : limit
+        const skip = (page-1)*limit
+
+        const loggedInUser = req.user
+        
+        const connectionRequest = await ConnectionRequestModel.find({
+            $or:[
+                {fromUserId:loggedInUser._id},{toUserId:loggedInUser._id}
+            ]
+        }).select("fromUserId toUserId")
+
+        const hideUsersFromFeed = new Set()
+
+        connectionRequest.forEach((connectionReq)=>{
+            hideUsersFromFeed.add(connectionReq.fromUserId.toString())
+            hideUsersFromFeed.add(connectionReq.toUserId.toString())
+        })
+        //array.from converts the anyother data structure to array(like in this,set is converted to array)
+        const users = await User.find({
+           $and:[{_id:{$nin:Array.from(hideUsersFromFeed)}},//$nin means not in
+                 {_id:{$ne:loggedInUser._id}}
+           ]  
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit)
+    
+        res.send(users)
     }catch(err){
         res.status(400).send("ERROR: " + err.message)
     }
